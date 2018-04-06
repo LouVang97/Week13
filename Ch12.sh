07cda0c204958cad9767b466df81c32851bb1ed7
LouVang97 <35350104+LouVang97@users.noreply.github.com>
2018-04-06 15:07:17 -0700

    Create Ch12.sh

---
+++ Ch12.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+# Chapter12 - Moving files across the network 
+
+echo "Here are the two files I had created....."
+echo 
+touch file1 file2
+sleep 2 
+
+echo "rsync file1 file2 ... host: "
+echo 
+$HOMEDIRCTORY 
+sleep 2 
+
+
+
