3722ea7163fa6fe0b14969186f77fc40ee547474
LouVang97 <35350104+LouVang97@users.noreply.github.com>
2018-04-06 22:43:40 +0000

    Update Ch12.sh

--- Ch12.sh
+++ Ch12.sh
@@ -1,15 +1,16 @@
 #!/bin/bash 
 # Chapter12 - Moving files across the network 
 
-echo "Here are the two files I had created....."
+echo "Here are the two files I created....."
 echo 
 touch file1 file2 
 echo
 sleep 2 
 
-echo "rsync file1 file2 ... host: cd .."
+echo "rsync file1 file2 ... host: $cd .."
 echo 
 sleep 2 
+
 
 
 
