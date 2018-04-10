b3681f661bed49cb01abf63fa438bbd889f74f3d
LouVang97 <35350104+LouVang97@users.noreply.github.com>
2018-04-10 09:59:15 -0700

    Update Ch12.sh

--- Ch12.sh
+++ Ch12.sh
@@ -1,12 +1,27 @@
 #!/bin/bash 
 # Chapter12 - Moving files across the network 
 
-echo "Here are the two files I created....."; echo ""; touch file1 file2; sleep 5; echo "";
-echo "rsync file1 file2 ... host: "; echo ""; cd ..; sleep 5; echo ""; 
-echo "rsync file1 fil2 ... user@host: "; echo ""; adduser lvang; sleep 5; echo "";
-echo "transfer entrie directory hirearchies"; echo ""; rsync -a dir host:destination_dir; sleep 5; echo "";
-echo "copy directories"; echo ""; rsync -nva dir host:destination_dir; sleep 5; echo ""; 
-echo "delete files in the destination directory that do not exist in the source directory"; echo ""; rsync -a --delete dir host:destioation_dir;
-sleep 5; echo ""; 
-
+echo "To get rsync working between two hosts, the rsync program must be installed on both the source and destination"
+echo ""
+sudo apt install rsync 
+echo "Here are the two file I created"
+echo ""
+touch file1 file2
+echo ""
+ls
+echo "transfer files"
+echo ""
+rsync file1 file2 ... host:
+echo "transfer entrie directory hirearchies"
+echo ""
+rsync -a dir host:destination_dir
+sleep 5
+echo "copy directories"
+echo ""
+rsync -nva dir host:destination_dir
+sleep 5
+echo "delete files in the destination directory that do not exist in the source directory";
+echo "" 
+rsync -a --delete dir host:destioation_dir;
+sleep 5
 
